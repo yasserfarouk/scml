3e317990518d049f78b46488c8d77c68ff40fb03
Yasser Mohammad <yasserfarouk@gmail.com>
2019-12-08 14:28:26 +0900

    Bump version: 0.1.2 → 0.1.3

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def read(*names, **kwargs):
 
 setup(
     name='scml',
-    version='0.1.2',
+    version='0.1.3',
     description='ANAC Supply Chain Management League Platform',
     long_description='%s\n%s' % (
         re.compile('^.. start-badges.*^.. end-badges', re.M | re.S).sub('', read('README.rst')),
