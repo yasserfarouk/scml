e671d001440aabe76ff9957bfd007f5ab6795be8
Yasser Mohammad <yasserfarouk@gmail.com>
2020-01-23 18:32:50 +0900

    v0.2.1

--- src/scml/__init__.py
+++ src/scml/__init__.py
@@ -1,6 +1,6 @@
 __author__ = """Yasser Mohammad"""
 __email__ = "yasserfarouk@gmail.com"
-__version__ = "__version__ = '0.2.0'"
+__version__ = "0.2.1"
 
 from .scml2019 import *
 from .scml2020 import *
