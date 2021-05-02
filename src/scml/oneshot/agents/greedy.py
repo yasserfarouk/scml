ab02fdc092d5d3b819c2fc9a55b8b395a4def0c9
Yasser Mohammad <yasserfarouk@gmail.com>
2021-05-02 15:31:19 +0900

    [oneshot] improving the GreedyOneShotAgent

--- src/scml/oneshot/agents/greedy.py
+++ src/scml/oneshot/agents/greedy.py
@@ -1,50 +1,148 @@
-import random
-from typing import Dict, Optional
+from collections import defaultdict
+from typing import Optional
 
-from negmas import MechanismState
+from negmas import Outcome
 from negmas import ResponseType
 from negmas import SAOResponse
-from negmas import SAOState, Outcome
-from negmas import SAOSingleAgreementRandomController, SAOSingleAgreementController
+from negmas import SAOSingleAgreementController
+from negmas import SAOSingleAgreementRandomController
 
-from scml.oneshot.agent import (
-    OneShotAgent,
-    OneShotSingleAgreementAgent,
-    OneShotSyncAgent,
-)
+from scml.oneshot.agent import OneShotAgent
+from scml.oneshot.agent import OneShotSingleAgreementAgent
 from scml.oneshot.agent import OneShotSyncAgent
-from scml.scml2020.common import QUANTITY, TIME, UNIT_PRICE
+from scml.scml2020.common import QUANTITY
+from scml.scml2020.common import TIME
+from scml.scml2020.common import UNIT_PRICE
 
 __all__ = ["GreedyOneShotAgent", "GreedySyncAgent", "GreedySingleAgreementAgent"]
 
 
 class GreedyOneShotAgent(OneShotAgent):
-    """A greedy agent based on OneShotAgent"""
+    """
+    A greedy agent based on OneShotAgent
+
+    Args:
+        concession_exponent: A real number controlling how fast does the agent
+                             concede on price.
+        acc_price_slack: The allowed slack in price limits compared with best
+                         prices I got so far
+        step_price_slack: The allowed slack in price limits compared with best
+                         prices I got this step
+        opp_price_slack: The allowed slack in price limits compared with best
+                         prices I got so far from a given opponent in this step
+        opp_acc_price_slack: The allowed slack in price limits compared with best
+                         prices I got so far from a given opponent so far
+    Remarks:
+        - A `concession_exponent` greater than one makes the agent concede
+          super linearly and vice versa
+
+    """
+
+    def __init__(
+        self,
+        *args,
+        concession_exponent=0.2,
+        acc_price_slack=float("inf"),
+        step_price_slack=0.1,
+        opp_price_slack=0.2,
+        opp_acc_price_slack=0.2,
+        range_slack=0.05,
+        **kwargs
+    ):
+        super().__init__(*args, **kwargs)
+        self._e = concession_exponent
+        self._acc_price_slack = acc_price_slack
+        self._step_price_slack = step_price_slack
+        self._opp_price_slack = opp_price_slack
+        self._opp_acc_price_slack = opp_acc_price_slack
+        self._range_slack = range_slack
 
     def init(self):
+        """Initialize the quantities and best prices received so far"""
+        self._best_selling, self._best_buying = 0.0, float("inf")
+        self._best_acc_selling, self._best_acc_buying = 0.0, float("inf")
+        self._best_opp_selling = defaultdict(float)
+        self._best_opp_buying = defaultdict(lambda: float("inf"))
+        self._best_opp_acc_selling = defaultdict(float)
+        self._best_opp_acc_buying = defaultdict(lambda: float("inf"))
         self._sales = self._supplies = 0
 
     def step(self):
+        """Initialize the quantities and best prices received for next step"""
+        self._best_selling, self._best_buying = 0.0, float("inf")
+        self._best_opp_selling = defaultdict(float)
+        self._best_opp_buying = defaultdict(lambda: float("inf"))
         self._sales = self._supplies = 0
 
     def on_negotiation_success(self, contract, mechanism):
-        if contract.annotation["product"] == self.awi.my_input_product:
-            self._sales += contract.agreement["quantity"]
-        else:
-            self._supplies += contract.agreement["quantity"]
+        """Record sales/supplies secured"""
+        super().on_negotiation_success(contract, mechanism)
 
-    def propose(self, negotiator_id: str, state) -> "Outcome":
-        return self.best_offer(negotiator_id)
+        # update my current best price to use for limiting concession in other
+        # negotiations
+        up = contract.agreement["unit_price"]
+        if contract.annotation["product"] == self.awi.my_output_product:
+            partner = contract.annotation["buyer"]
+            self._best_acc_selling = max(up, self._best_acc_selling)
+            self._best_opp_acc_selling[partner] = max(
+                up, self._best_opp_acc_selling[partner]
+            )
+        else:
+            partner = contract.annotation["seller"]
+            self._best_acc_buying = min(up, self._best_acc_buying)
+            self._best_opp_acc_buying[partner] = min(
+                up, self._best_opp_acc_buying[partner]
+            )
+
+    def propose(self, negotiator_id: str, state):
+        # find the absolute best offer for me. This will most likely has an
+        # unrealistic price
+        offer = self.best_offer(negotiator_id)
+
+        # if there are no best offers, just return None to end the negotiation
+        if not offer:
+            return None
+
+        # over-write the unit price in the best offer with a good-enough price
+        offer = list(offer)
+        offer[UNIT_PRICE] = self._find_good_price(self.get_ami(negotiator_id), state)
+        return tuple(offer)
 
     def respond(self, negotiator_id, state, offer):
+        # find the quantity I still need and end negotiation if I need nothing more
         my_needs = self._needed(negotiator_id)
         if my_needs <= 0:
             return ResponseType.END_NEGOTIATION
-        return (
+
+        # reject any offers with quantities above my needs
+        response = (
             ResponseType.ACCEPT_OFFER
             if offer[QUANTITY] <= my_needs
             else ResponseType.REJECT_OFFER
         )
+        if response != ResponseType.ACCEPT_OFFER:
+            return response
+
+        # reject offers with prices that are deemed NOT good-enough
+        ami = self.get_ami(negotiator_id)
+        response = (
+            response
+            if self._is_good_price(ami, state, offer[UNIT_PRICE])
+            else ResponseType.REJECT_OFFER
+        )
+
+        # update my current best price to use for limiting concession in other
+        # negotiations
+        up = offer[UNIT_PRICE]
+        if self._is_selling(ami):
+            self._best_selling = max(up, self._best_selling)
+            partner = ami.annotation["buyer"]
+            self._best_opp_selling[partner] = max(up, self._best_selling)
+        else:
+            self._best_buying = min(up, self._best_buying)
+            partner = ami.annotation["seller"]
+            self._best_opp_buying[partner] = min(up, self._best_buying)
+        return response
 
     def best_offer(self, negotiator_id):
         my_needs = self._needed(negotiator_id)
@@ -79,6 +177,76 @@ class GreedyOneShotAgent(OneShotAgent):
         if not ami:
             return None
         return ami.annotation["product"] == self.awi.my_output_product
+
+    def _is_good_price(self, ami, state, price):
+        """Checks if a given price is good enough at this stage"""
+        mn, mx = self._price_range(ami)
+        th = self._th(state.step, ami.n_steps)
+        # a good price is one better than the threshold
+        if self._is_selling(ami):
+            return (price - mn) >= th * (mx - mn)
+        else:
+            return (mx - price) >= th * (mx - mn)
+
+    def _find_good_price(self, ami, state):
+        """Finds a good-enough price conceding linearly over time"""
+        mn, mx = self._price_range(ami)
+        th = self._th(state.step, ami.n_steps)
+        # offer a price that is around th of your best possible price
+        if self._is_selling(ami):
+            return mn + th * (mx - mn)
+        else:
+            return mx - th * (mx - mn)
+
+    def _price_range(self, ami):
+        """Limits the price by the best price received"""
+        mn = ami.issues[UNIT_PRICE].min_value
+        mx = ami.issues[UNIT_PRICE].max_value
+        if self._is_selling(ami):
+            partner = ami.annotation["buyer"]
+            mn = min(
+                mx * (1 - self._range_slack),
+                max(
+                    [mn]
+                    + [
+                        p * (1 - slack)
+                        for p, slack in (
+                            (self._best_selling, self._step_price_slack),
+                            (self._best_acc_selling, self._acc_price_slack),
+                            (self._best_opp_selling[partner], self._opp_price_slack),
+                            (
+                                self._best_opp_acc_selling[partner],
+                                self._opp_acc_price_slack,
+                            ),
+                        )
+                    ]
+                ),
+            )
+        else:
+            partner = ami.annotation["seller"]
+            mx = max(
+                mn * (1 + self._range_slack),
+                min(
+                    [mx]
+                    + [
+                        p * (1 - slack)
+                        for p, slack in (
+                            (self._best_buying, self._step_price_slack),
+                            (self._best_acc_buying, self._acc_price_slack),
+                            (self._best_opp_buying[partner], self._opp_price_slack),
+                            (
+                                self._best_opp_acc_buying[partner],
+                                self._opp_acc_price_slack,
+                            ),
+                        )
+                    ]
+                ),
+            )
+        return mn, mx
+
+    def _th(self, step, n_steps):
+        """calculates a descending threshold (0 <= th <= 1)"""
+        return ((n_steps - step - 1) / (n_steps - 1)) ** self._e
 
 
 class GreedySyncAgent(OneShotSyncAgent, GreedyOneShotAgent):
