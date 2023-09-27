e999c2c4aa86929119fe7a6907026aac93e5be34
Yasser Mohammad <yasserfarouk@gmail.com>
2023-09-27 09:37:54 -0500

    importing observations

--- src/scml/oneshot/rl/__init__.py
+++ src/scml/oneshot/rl/__init__.py
@@ -3,7 +3,13 @@ from .agent import *
 from .env import *
 from .factory import *
 from .observation import *
+from .policies import *
 
 __all__ = (
-    action.__all__ + observation.__all__ + agent.__all__ + env.__all__ + factory.__all__
+    action.__all__
+    + observation.__all__
+    + agent.__all__
+    + env.__all__
+    + factory.__all__
+    + policies.__all__
 )
