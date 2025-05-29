5a952e51e5fdadffcafa531e0f5edff9cc95d59e
Yasser Mohammad <yasserfarouk@gmail.com>
2025-05-29 10:28:29 +0900

    v0.7.7

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def read(*names, **kwargs):
 
 setup(
     name="scml",
-    version="0.7.6",
+    version="0.7.7",
     description="ANAC Supply Chain Management League Platform",
     long_description="%s\n%s"
     % (
